38891b941f87dfcc3881031f59e0c2e371655ec3
github username <github email address>
2019-11-14 15:15:10 -0700

    prevent duplicate user creation

--- api/users.js
+++ api/users.js
@@ -7,27 +7,38 @@ function isValidId(request, response, next){
     next(new Error('Invalid ID'))
 }
 
-router.get('/', (request, response) => {
-    queries.getAllUsers().then(users => {
-        response.json(users)
-    })
-})
+// router.get('/', (request, response) => {
+//     queries.getAllUsers().then(users => {
+//         response.json(users)
+//     })
+// })
 
 router.get('/:id', isValidId, (request, response) => {
     queries.getUser(request.params.id).then(user => {
         if(user){
             response.json(user)
         } else {
-            response.status(404)
+            response.sendStatus(404)
             next()
         }
     })
 })
 
 router.post('/', (request, response) => {
-    queries.createUser(request.body).then(users => {
-        return response.json(users[0])
-    })
+    console.log(request.body)
+    if(
+        request.body.firstName && 
+        request.body.lastName && 
+        request.body.phoneNumber && 
+        request.body.email && 
+        request.body.password 
+    ){
+        queries.createUser(request.body).then(users => {
+            return response.json(users[0])
+        })
+    } else {
+        response.json({error: "Missing information"})
+    }
 })
 
 module.exports = router
